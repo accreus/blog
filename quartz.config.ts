5835d3871527655da180ee177dbfd513d0455940
accreus <jwalinpatel22@gmail.com>
2025-11-26 20:09:14 +0530

    Quartz sync: Nov 26, 2025, 8:09 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "https://accreus.github.io/blog",
+    baseUrl: "accreus.github.io/blog",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
